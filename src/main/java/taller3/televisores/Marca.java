254ad0d0fef8abba0648f2c3d62f50ca4fa14088
Julián Orozco V <jorozcova@unal.edu.co>
2022-03-26 12:22:50 -0500

    prueba1

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,17 @@
+package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+	
+	public Marca (String nomb) {
+		nombre = nomb;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nomb) {
+		nombre = nomb;
+	}
+}
